5141cb0d17ba807aedda8242c917433e0bfa18c7
adelppi <adelppi.baig@gmail.com>
2024-11-14 02:21:29 +0900

    コメント書いた

--- game.py
+++ game.py
@@ -8,6 +8,9 @@ class Map:
         self.array = np.zeros(dtype=int, shape=(height, width))
 
     def set(self, pos, value):
+        """
+        Mapのposの座標に値をセットする
+        """
         self.array[pos[1]][pos[0]] = value
 
 
@@ -19,9 +22,16 @@ class Player:
 
     @property
     def pos(self):
+        """
+        Playerのposを返す
+            return: np.ndarray
+        """
         return np.array([self.x, self.y])
 
     def set(self, pos):
+        """
+        Playerの位置をセットする
+        """
         self.x = pos[0]
         self.y = pos[1]
 
@@ -33,11 +43,18 @@ class Game:
         self.players = {}
 
     def add_player(self, name, initial_pos):
+        """
+        新しいPlayerを追加する
+        """
         new_player = Player(name, initial_pos[0], initial_pos[1])
         self.players[name] = new_player
         self.place_player(new_player.pos)
 
     def move_player(self, name, dir):
+        """
+        w, a, s, dをdirとして受け取って、指定したPlayerをその方向に動かす
+        """
+
         def movement(d):
             match d:
                 case "w":
@@ -55,15 +72,36 @@ class Game:
         self.player_map.set(player.pos, 1)
 
     def place_object(self, pos, object):
+        """
+        指定したposにオブジェクトを置く
+        """
+
         self.field.set(pos, object)
 
     def place_player(self, pos):
+        """
+        指定したposにPlayerを置く
+        """
         self.player_map.set(pos, 1)
 
+    def delete_player(self, name):
+        """
+        Playerを削除する
+        """
+        self.player_map.set(self.players[name].pos, 0)
+        del self.players[name]
+
     def compile_layers(self):
+        """
+        クライアントに送るMap配列(fieldとplayer_mapを合わせたもの)
+        を生成する
+        """
         compiled_layer
 
     def print_field(self):
+        """
+        デバッグ用のプリント
+        """
         print("Field:")
         print(self.field.array)
         print("\nPlayer Positions:")
@@ -82,9 +120,13 @@ if __name__ == "__main__":
     game.add_player("mio", (6, 5))
 
     game.print_field()
-    # print(game.players)
+    print(game.players)
 
     game.move_player("adi", "w")
     game.move_player("adi", "a")
     game.move_player("mio", "a")
     game.print_field()
+
+    game.delete_player("mio")
+    game.print_field()
+    print(game.players)
